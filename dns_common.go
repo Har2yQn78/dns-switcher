9d6494f51eae350d5d77093a05e1466b2f075447
Har2yQn78 <hamidreza.amiri800@gmail.com>
2026-02-21 18:55:41 +0330

    fix build for windows

--- dns_common.go
+++ dns_common.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"sort"
+	"strings"
 	"time"
 )
 
@@ -59,4 +61,70 @@ func TestDNSLatency(server string) int {
 	}
 
 	return int(elapsed)
+}
+
+func TestAllProviders() {
+	for i := range providers {
+		if providers[i].Name == "Reset to Default" || providers[i].Name == "Add Custom DNS" {
+			providers[i].Latency = -1
+			continue
+		}
+
+		if len(providers[i].Servers) > 0 {
+			latency := TestDNSLatency(providers[i].Servers[0])
+			providers[i].Latency = latency
+		}
+	}
+}
+
+func SortProvidersByLatency() {
+	var special []DNSProvider
+	var normal []DNSProvider
+
+	for _, p := range providers {
+		if p.Name == "Reset to Default" || p.Name == "Add Custom DNS" {
+			special = append(special, p)
+		} else {
+			normal = append(normal, p)
+		}
+	}
+
+	sort.Slice(normal, func(i, j int) bool {
+		if normal[i].Latency == -1 {
+			return false
+		}
+		if normal[j].Latency == -1 {
+			return true
+		}
+		return normal[i].Latency < normal[j].Latency
+	})
+
+	providers = append(normal, special...)
+}
+
+func formatDuration(seconds int) string {
+	if seconds < 60 {
+		return fmt.Sprintf("%ds", seconds)
+	}
+	minutes := seconds / 60
+	secs := seconds % 60
+	if minutes < 60 {
+		return fmt.Sprintf("%dm %ds", minutes, secs)
+	}
+	hours := minutes / 60
+	mins := minutes % 60
+	return fmt.Sprintf("%dh %dm", hours, mins)
+}
+
+func parseCustomDNS(input string) []string {
+	input = strings.ReplaceAll(input, ",", " ")
+	parts := strings.Fields(input)
+	var servers []string
+	for _, part := range parts {
+		part = strings.TrimSpace(part)
+		if part != "" {
+			servers = append(servers, part)
+		}
+	}
+	return servers
 }
--- main.go
+++ main.go
@@ -5,8 +5,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"sort"
-	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -34,7 +32,7 @@ func printBox(title string, content []string) {
 	width := 60
 
 	fmt.Println(boxStyle.Render("  ┌" + repeatStr("─", width-4) + "┐"))
-	fmt.Println(boxStyle.Render("  │ ") + labelStyle.Render(title) + boxStyle.Render(repeatStr(" ", width-len(title)-6) + "│"))
+	fmt.Println(boxStyle.Render("  │ ") + labelStyle.Render(title) + boxStyle.Render(repeatStr(" ", width-len(title)-6)+"│"))
 	fmt.Println(boxStyle.Render("  ├" + repeatStr("─", width-4) + "┤"))
 
 	for _, line := range content {
@@ -42,7 +40,7 @@ func printBox(title string, content []string) {
 		if padding < 0 {
 			padding = 0
 		}
-		fmt.Println(boxStyle.Render("  │ ") + line + boxStyle.Render(repeatStr(" ", padding) + "│"))
+		fmt.Println(boxStyle.Render("  │ ") + line + boxStyle.Render(repeatStr(" ", padding)+"│"))
 	}
 
 	fmt.Println(boxStyle.Render("  └" + repeatStr("─", width-4) + "┘"))
@@ -78,72 +76,6 @@ func stripANSI(str string) string {
 		result += " "
 	}
 	return result
-}
-
-func TestAllProviders() {
-	for i := range providers {
-		if providers[i].Name == "Reset to Default" || providers[i].Name == "Add Custom DNS" {
-			providers[i].Latency = -1
-			continue
-		}
-
-		if len(providers[i].Servers) > 0 {
-			latency := TestDNSLatency(providers[i].Servers[0])
-			providers[i].Latency = latency
-		}
-	}
-}
-
-func SortProvidersByLatency() {
-	var special []DNSProvider
-	var normal []DNSProvider
-
-	for _, p := range providers {
-		if p.Name == "Reset to Default" || p.Name == "Add Custom DNS" {
-			special = append(special, p)
-		} else {
-			normal = append(normal, p)
-		}
-	}
-
-	sort.Slice(normal, func(i, j int) bool {
-		if normal[i].Latency == -1 {
-			return false
-		}
-		if normal[j].Latency == -1 {
-			return true
-		}
-		return normal[i].Latency < normal[j].Latency
-	})
-
-	providers = append(normal, special...)
-}
-
-func formatDuration(seconds int) string {
-	if seconds < 60 {
-		return fmt.Sprintf("%ds", seconds)
-	}
-	minutes := seconds / 60
-	secs := seconds % 60
-	if minutes < 60 {
-		return fmt.Sprintf("%dm %ds", minutes, secs)
-	}
-	hours := minutes / 60
-	mins := minutes % 60
-	return fmt.Sprintf("%dh %dm", hours, mins)
-}
-
-func parseCustomDNS(input string) []string {
-	input = strings.ReplaceAll(input, ",", " ")
-	parts := strings.Fields(input)
-	var servers []string
-	for _, part := range parts {
-		part = strings.TrimSpace(part)
-		if part != "" {
-			servers = append(servers, part)
-		}
-	}
-	return servers
 }
 
 func main() {
